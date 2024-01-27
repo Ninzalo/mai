4377530ba8b1e37a2ab585ba89c0954b511cb42e
Ninzalo <k-artemiy@mail.ru>
2024-01-28 00:13:45 +0300

    fix(header): revert header image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,15 @@
 import "./Header.css";
+import headerBackground from "../../images/header/1.jpg";
 
 export default function Header() {
   return (
     <div className="header">
+      <img
+        src={headerBackground}
+        alt="header background"
+        style={{ width: "100%" }}
+      />
       <div className="info-block">
-        <div className="logo"></div>
         <h1 className="main-text">Лаборатория №4</h1>
         <h1 className="main-text">Аддитивные технологии</h1>
         <h2 className="sub-text"></h2>
